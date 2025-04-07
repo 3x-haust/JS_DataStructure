372f372aff97620f5129d4ee619afdfc80012673
유성윤 <94370559+3x-haust@users.noreply.github.com>
2025-04-07 13:37:28 +0900

    재귀 함수 예제 파일 추가

---
+++ recursion.mjs
@@ -0,0 +1,12 @@
+// function myFunction(number) {
+//   if (number > 10) return;
+//   console.log(number);
+//   myFunction(number + 1);
+// }
+
+// myFunction(1);
+
+// for문을 이용해서 1~10까지 출력
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
